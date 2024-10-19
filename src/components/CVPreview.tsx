f8f2da0338d9cd5b9528d2eb526aeb2b0670be5d
Kyla <kflmorcillos@gmail.com>
2024-10-19 23:10:57 +0800

    Add PersonalDetailsInfo component

--- src/components/CVPreview.tsx
+++ src/components/CVPreview.tsx
@@ -1,3 +1,19 @@
+function PersonalDetailsInfo() {
+    return(
+        <div className="pt-8 pl-8 pr-8 pb-8 text-sm">
+            <p className="font-bold text-4xl mb-1">John Smith</p>
+            <p className="text-lg mb-6">IT Project Manager</p>
+            <div className="grid grid-cols-2 grid-rows-2 mb-6 gap-y-2" style={{ width: '500px' }}>
+                <p className="inline">774-987-4009</p>
+                <p className="inline">j.smith@uptowork.com</p>
+                <p className="inline">j.smith@uptowork.com</p>
+                <p className="inline">j.smith@uptowork.com</p>
+            </div>
+            <p className="">IT Professional with over 10 years of experience specializing in IT department management for international logistics companies. I can implement effective IT strategies at local and global levels. My greatest strength is business awareness, which enables me to permanently streamline infrastructure and applications. Seeking to leverage my IT management skills at Sancorp Inc.</p>
+        </div>
+    )
+}
+
 function CVPreview() {
     return(
         <div className="float-right"
@@ -6,11 +22,7 @@ function CVPreview() {
                 height: '1122.5px',
                 boxShadow: '1px 2px 18px 0px rgba(0,0,0,0.69)'
             }}>
-            <p>John Smith</p>
-            <p>IT Project Manager</p>
-            <p>774-987-4009</p>
-            <p>j.smith@uptowork.com</p>
-            <p>IT Professional with over 10 years of experience specializing in IT department management for international logistics companies. I can implement effective IT strategies at local and global levels. My greatest strength is business awareness, which enables me to permanently streamline infrastructure and applications. Seeking to leverage my IT management skills at Sancorp Inc.</p>
+            <PersonalDetailsInfo/>
         </div>
     );
 }
