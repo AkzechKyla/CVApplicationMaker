3f51dbcb2f31483557c6826ef067577248c53675
Kyla <kflmorcillos@gmail.com>
2024-10-19 18:15:16 +0800

    Change margin to padding

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import PersonalDetails from './components/CVForm/PersonalDetails'
 
 export default function App() {
     return (
-      <div className="flex gap-6 mt-8 mr-8 ml-8 mb-8 overflow-auto">
+      <div className="flex gap-6 pt-8 pr-8 pb-8 pl-8 overflow-auto">
         <div className="bg-emerald-500 flex-1">
           <Header/>
           <div className="flex gap-6">
