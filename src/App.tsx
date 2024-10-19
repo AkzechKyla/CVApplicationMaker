6bde348292640db1118437c382d7faefe2070b34
Kyla <kflmorcillos@gmail.com>
2024-10-19 15:01:27 +0800

    Add CV Preview div

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import CVPreview from './components/CVPreview'
+
 export default function App() {
     return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="mt-8 mr-8 mb-8 overflow-auto">
+        <CVPreview/>
+      </div>
     )
 }
---
+++ src/components/CVPreview.tsx
@@ -0,0 +1,9 @@
+function CVPreview() {
+    return(
+        <div className="bg-blue-700 float-right" style={{ width: '793.7px', height: '1122.5px' }}>
+            <p>CV Preview</p>
+        </div>
+    );
+}
+
+export default CVPreview;
