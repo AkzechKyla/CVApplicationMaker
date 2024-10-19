a122dda7fa6f22821130fa4cf00827d7b17c55c1
Kyla <kflmorcillos@gmail.com>
2024-10-19 22:09:21 +0800

    Add sample data

--- src/components/CVPreview.tsx
+++ src/components/CVPreview.tsx
@@ -6,7 +6,11 @@ function CVPreview() {
                 height: '1122.5px',
                 boxShadow: '1px 2px 18px 0px rgba(0,0,0,0.69)'
             }}>
-            <p>CV Preview</p>
+            <p>John Smith</p>
+            <p>IT Project Manager</p>
+            <p>774-987-4009</p>
+            <p>j.smith@uptowork.com</p>
+            <p>IT Professional with over 10 years of experience specializing in IT department management for international logistics companies. I can implement effective IT strategies at local and global levels. My greatest strength is business awareness, which enables me to permanently streamline infrastructure and applications. Seeking to leverage my IT management skills at Sancorp Inc.</p>
         </div>
     );
 }
