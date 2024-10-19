8dee7cd593cfb6f00c6cca958a8e6f1bc6baf70a
Kyla <kflmorcillos@gmail.com>
2024-10-19 18:15:32 +0800

    Add box shadow

--- src/components/CVPreview.tsx
+++ src/components/CVPreview.tsx
@@ -1,6 +1,11 @@
 function CVPreview() {
     return(
-        <div className="bg-blue-700 float-right" style={{ width: '793.7px', height: '1122.5px' }}>
+        <div className="float-right"
+            style={{
+                width: '793.7px',
+                height: '1122.5px',
+                boxShadow: '1px 2px 18px 0px rgba(0,0,0,0.69)'
+            }}>
             <p>CV Preview</p>
         </div>
     );
