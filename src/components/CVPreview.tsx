3019814c485c3b5b4960b40ec7f0f6709d530762
Kyla <kflmorcillos@gmail.com>
2024-12-01 21:01:33 +0800

    Move PersonalDetailsInfo to separate file

--- src/components/CVPreview.tsx
+++ src/components/CVPreview.tsx
@@ -1,18 +1,4 @@
-function PersonalDetailsInfo() {
-    return(
-        <div className="pt-8 pb-8 text-sm">
-            <p className="font-bold text-4xl mb-1">John Smith</p>
-            <p className="text-lg mb-6">IT Project Manager</p>
-            <div className="grid grid-cols-2 grid-rows-2 mb-6 gap-y-2 w-[31.25rem]">
-                <p className="inline text-sm">774-987-4009</p>
-                <p className="inline text-sm">j.smith@uptowork.com</p>
-                <p className="inline text-sm">j.smith@uptowork.com</p>
-                <p className="inline text-sm">j.smith@uptowork.com</p>
-            </div>
-            <p className="text-sm">IT Professional with over 10 years of experience specializing in IT department management for international logistics companies. I can implement effective IT strategies at local and global levels. My greatest strength is business awareness, which enables me to permanently streamline infrastructure and applications. Seeking to leverage my IT management skills at Sancorp Inc.</p>
-        </div>
-    )
-}
+import { PersonalDetailsInfo } from './CVPreview/PersonalDetailsInfo';
 
 function ExperienceInfo() {
     return <div>
