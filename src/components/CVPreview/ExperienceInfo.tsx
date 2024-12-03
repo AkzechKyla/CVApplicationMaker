633f08b92c4181b20a358ebf33e1a114e71f3b66
Kyla <kflmorcillos@gmail.com>
2024-12-03 19:42:41 +0800

    Move EducationInfo to separate file

---
+++ src/components/CVPreview/ExperienceInfo.tsx
@@ -0,0 +1,18 @@
+export function ExperienceInfo() {
+    return <div>
+        <h1 className="font-bold">Experience</h1>
+        <div className="flex">
+            <div>2006-12 - present</div>
+            <div>
+                <p>Senior Project Manager</p>
+                <p>Seton Hospital, ME</p>
+                <ul className="list-disc pl-8">
+                    <li>Oversaw all major hospital IT projects for 10+ years, focus on cost reduction.</li>
+                    <li>Responsible for creating, improving, and developing IT project strategies.</li>
+                    <li>Implemented the highly successful Lean Training and Six Sigma projects for all employees. Cut costs by 32%.</li>
+                    <li>I reduced the costs of IT infrastructure maintenance by 5% in 2015 by successfully rebuilding the infrastructure.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+}
