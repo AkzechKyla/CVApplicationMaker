93a77f1234251518eb0fbe33cfc8879cd8119a72
Kyla <kflmorcillos@gmail.com>
2024-12-05 23:35:42 +0800

    Add Education info

--- src/pages/CVPreview.tsx
+++ src/pages/CVPreview.tsx
@@ -1,11 +1,29 @@
 import { PersonalDetailsInfo } from '../components/CVPreview/PersonalDetailsInfo';
 import { ExperienceInfo } from '../components/CVPreview/ExperienceInfo';
 
-function EducationInfo() {
+// function EducationInfo() {
+//     return <div>
+//         <h1 className="font-bold">Education</h1>
+//     </div>
+// }
+
+export function EducationInfo() {
     return <div>
         <h1 className="font-bold">Education</h1>
+        <div className="flex">
+            <div>1996-09 - 2001-05</div>
+            <div>
+                <p>BS/MS in Computer Science, University of Maryland</p>
+                <ul className="list-disc pl-8">
+                    <li>Graduated Summa Cum Laude</li>
+                    <li>Member of Student Association of Project Management</li>
+                    <li>Managed a student project to develop weekly IT News podcast.</li>
+                </ul>
+            </div>
+        </div>
     </div>
 }
+
 
 function CVPreview() {
     return(
