a39c3761b1e2db9b526a540f77d169f175f88678
Kyla <kflmorcillos@gmail.com>
2024-12-14 20:47:41 +0800

    Move ExperienceInfo to separate file

---
+++ src/components/CVPreview/EducationInfo.tsx
@@ -0,0 +1,16 @@
+export function EducationInfo() {
+    return <div>
+        <h1 className="font-bold">Education</h1>
+        <div className="flex w-full">
+            <div className="w-2/6">1996-09 - 2001-05</div>
+            <div className="w-full">
+                <p>BS/MS in Computer Science, University of Maryland</p>
+                <ul className="list-disc pl-8">
+                    <li>Graduated Summa Cum Laude</li>
+                    <li>Member of Student Association of Project Management</li>
+                    <li>Managed a student project to develop weekly IT News podcast.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+}
