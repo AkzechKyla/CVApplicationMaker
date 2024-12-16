3a5300caca9124b714c7c16df4b3be2ce83d90a5
Kyla <kflmorcillos@gmail.com>
2024-12-16 11:15:06 +0800

    Change style

--- src/pages/CVPreview.tsx
+++ src/pages/CVPreview.tsx
@@ -4,10 +4,8 @@ import { EducationInfo } from '../components/CVPreview/EducationInfo';
 
 function CVPreview() {
     return(
-        <div className="float-right pl-8 pr-8"
+        <div className="float-right pl-8 pr-8 w-[49.606rem] h-[70.156rem] shadow-2xl"
             style={{
-                width: '793.7px',
-                height: '1122.5px',
                 boxShadow: '1px 2px 18px 0px rgba(0,0,0,0.69)'
             }}>
             <PersonalDetailsInfo/>
