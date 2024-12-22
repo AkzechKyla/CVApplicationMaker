ff00e87d5af28ddaff30a3aa077e2aff5c3bb3d9
Kyla <kflmorcillos@gmail.com>
2024-12-22 22:07:30 +0800

    Add stylings

--- src/pages/CVPreview.tsx
+++ src/pages/CVPreview.tsx
@@ -8,11 +8,11 @@ function SkillsInfo() {
             <h1 className="font-bold">Skills</h1>
             <div className="flex w-full">
                 <div className="w-2/6"></div>
-                <div className="w-full">
-                    <p>Business Process Improvement - history of successful innovations leading to cost savings.</p>
-                    <p>Vendor Management - proven track record of managing vendors in projects with budgets of over $1,000,000.</p>
-                    <p>Project Scheduling - over 90% of projects led were finished in due time.</p>
-                    <p>Sales Analysis - background in IT Sales with deep understanding of negotiating contracts.</p>
+                <div className="w-full text-base">
+                    <p><b>Business Process Improvement</b> - history of successful innovations leading to cost savings.</p>
+                    <p><b>Vendor Management</b> - proven track record of managing vendors in projects with budgets of over $1,000,000.</p>
+                    <p><b>Project Scheduling</b> - over 90% of projects led were finished in due time.</p>
+                    <p><b>Sales Analysis</b> - background in IT Sales with deep understanding of negotiating contracts.</p>
                 </div>
             </div>
 
