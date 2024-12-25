d9411a5926f3cfbf6042c6d5e91216384e4899fb
Kyla <kflmorcillos@gmail.com>
2024-12-25 17:29:08 +0800

    Move SoftwareInfo to separate file

---
+++ src/components/CVPreview/SoftwareInfo.tsx
@@ -0,0 +1,14 @@
+export function SoftwareInfo() {
+    return(
+        <div>
+            <h1 className="font-bold">Software</h1>
+            <div className="flex w-full">
+                <div className="w-2/6"></div>
+                <div className="w-full text-base">
+                    <p>Microsoft Project, MS Windows Server, Linux/Unix</p>
+                </div>
+                <div className="w-2/6">Excellent</div>
+            </div>
+        </div>
+    );
+}
--- src/pages/CVPreview.tsx
+++ src/pages/CVPreview.tsx
@@ -2,21 +2,7 @@ import { PersonalDetailsInfo } from '../components/CVPreview/PersonalDetailsInfo
 import { ExperienceInfo } from '../components/CVPreview/ExperienceInfo';
 import { EducationInfo } from '../components/CVPreview/EducationInfo';
 import { SkillsInfo } from '../components/CVPreview/SkillsInfo';
-
-function SoftwareInfo() {
-    return(
-        <div>
-            <h1 className="font-bold">Software</h1>
-            <div className="flex w-full">
-                <div className="w-2/6"></div>
-                <div className="w-full text-base">
-                    <p>Microsoft Project, MS Windows Server, Linux/Unix</p>
-                </div>
-                <div className="w-2/6">Excellent</div>
-            </div>
-        </div>
-    );
-}
+import { SoftwareInfo } from '../components/CVPreview/SoftwareInfo';
 
 function CVPreview() {
     return(
