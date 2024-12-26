8f00aac8b2d61edcaeec7176b09d399c2c098d8e
Kyla <kflmorcillos@gmail.com>
2024-12-26 21:37:41 +0800

    Add more Software Info details

--- src/components/CVPreview/SoftwareInfo.tsx
+++ src/components/CVPreview/SoftwareInfo.tsx
@@ -4,10 +4,20 @@ export function SoftwareInfo() {
             <h1 className="font-bold">Software</h1>
             <div className="flex w-full">
                 <div className="w-2/6"></div>
-                <div className="w-full text-base">
-                    <p>Microsoft Project, MS Windows Server, Linux/Unix</p>
+                <div className="w-full flex-row">
+                    <div className="flex text-base">
+                        <p className="w-full">Microsoft Project, MS Windows Server, Linux/Unix</p>
+                        <p className="w-2/6">Excellent</p>
+                    </div>
+                    <div className="flex text-base">
+                        <p className="w-full">MS Window Server</p>
+                        <p className="w-2/6">Very Good</p>
+                    </div>
+                    <div className="flex text-base">
+                        <p className="w-full">Linux/Unix</p>
+                        <p className="w-2/6">Very Good</p>
+                    </div>
                 </div>
-                <div className="w-2/6">Excellent</div>
             </div>
         </div>
     );
