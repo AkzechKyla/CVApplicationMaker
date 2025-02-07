5d54add81aa4c13a3a3b4ae2dc0e5eb798d1ab28
Kyla <kflmorcillos@gmail.com>
2025-02-07 22:37:18 +0800

    Adjust padding

--- src/pages/CVPreview.tsx
+++ src/pages/CVPreview.tsx
@@ -6,7 +6,7 @@ import { SoftwareInfo } from '../components/CVPreview/SoftwareInfo';
 
 function CVPreview() {
     return(
-        <div className="float-right pl-8 pr-8 w-[49.606rem] h-[70.156rem] shadow-2xl"
+        <div className="float-right p-8 w-[49.606rem] h-[70.156rem] shadow-2xl"
             style={{
                 boxShadow: '1px 2px 18px 0px rgba(0,0,0,0.69)'
             }}>
