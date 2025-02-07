a87c212e7815207b636106ae2e9a3f43d7793e97
Kyla <kflmorcillos@gmail.com>
2025-02-08 00:43:50 +0800

    Change background color

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import PersonalDetails from './components/CVForm/PersonalDetails'
 
 export default function App() {
     return (
-      <div className="flex gap-6 pt-8 pr-8 pb-8 pl-8 overflow-auto font-helvetica">
-        <div className="bg-emerald-500 flex-1">
-          <Header/>
+      <div className="flex gap-6 p-8 bg-zinc-100 overflow-auto font-helvetica">
+        <div className="flex-1">
+          <Title/>
           <div className="flex gap-6">
             <Sidebar/>
             <PersonalDetails/>
--- src/components/CVForm/PersonalDetails.tsx
+++ src/components/CVForm/PersonalDetails.tsx
@@ -1,6 +1,6 @@
 function PersonalDetails() {
     return(
-        <div className="personal-details-form bg-amber-400 max-w-full flex-1">
+        <div className="personal-details-form max-w-full flex-1">
             <p>Personal Details Form</p>
         </div>
     );
