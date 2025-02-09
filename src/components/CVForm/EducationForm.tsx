57efc08a6c54a23ed13a8ae3a9d70a506448bd75
Kyla <kflmorcillos@gmail.com>
2025-02-10 00:07:01 +0800

    EducationForm: Add style

--- src/components/CVForm/EducationForm.tsx
+++ src/components/CVForm/EducationForm.tsx
@@ -1,18 +1,59 @@
 function EducationForm() {
     return(
         <div className="bg-white max-w-full flex-1 p-3 drop-shadow-md rounded-lg space-y-3">
-            <p>Education</p>
+            <h1 className="font-bold">Education</h1>
+
             <div className="flex justify-between">
-                <p>Education 1</p>
-                <p>Delete</p>
+                <p className="text-base font-bold">Education 1</p>
+                <button>
+                    <span className="material-icons text-red-600">delete</span>
+                </button>
             </div>
-            <p>Degree</p>
-            <p>School</p>
-            <p>Start Date</p>
-            <p>End Date</p>
-            <p>Present</p>
-            <p>+Add Description</p>
-            <p>+Add Education</p>
+
+            <form className="space-y-2">
+                <div>
+                    <label className="block text-sm font-bold">Degree</label>
+                    <input type="text" className="input-box" />
+                </div>
+                <div>
+                    <label className="block text-sm font-bold">School Name</label>
+                    <input type="text" className="input-box" />
+                </div>
+                <div className="flex gap-4 items-center">
+                    <div>
+                        <label className="block text-sm font-bold">Start Date</label>
+                        <input type="date" className="input-box" />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-bold">End Date</label>
+
+                        <div className="flex items-center gap-4">
+                            <input type="date" className="input-box" />
+
+                            <div className="flex gap-1">
+                                <input type="checkbox" id="present" />
+                                <label htmlFor="present" className="text-sm font-bold">Present</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <label className="block text-sm font-bold">Description</label>
+                    <textarea className="input-box"></textarea>
+                </div>
+            </form>
+
+            <p className="block text-sm font-bold text-blue-600 hover:underline cursor-pointer">
+                <span>+ </span>
+                Add Description
+            </p>
+
+            <div className="flex justify-center">
+                <button className="add-button">
+                    <span className="text-lg">+</span> Education
+                </button>
+            </div>
         </div>
     );
 }
