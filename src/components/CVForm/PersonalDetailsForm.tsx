948a832335096112da7edb47cc14e21b4df84316
Kyla <kflmorcillos@gmail.com>
2025-02-10 00:03:24 +0800

    PersonalDetailsForm: Add styles

--- src/components/CVForm/PersonalDetailsForm.tsx
+++ src/components/CVForm/PersonalDetailsForm.tsx
@@ -1,13 +1,33 @@
 function PersonalDetailsForm() {
-    return(
+    return (
         <div className="bg-white max-w-full flex-1 p-3 drop-shadow-md rounded-lg space-y-3">
-            <p>Personal Details</p>
-            <p>Full Name</p>
-            <p>Position</p>
-            <p>Contact Number</p>
-            <p>Email Address</p>
-            <p>City and Province</p>
-            <p>Description</p>
+            <h1 className="font-bold">Personal Details</h1>
+            <form className="space-y-2">
+                <div>
+                    <label className="block text-sm font-bold">Full Name</label>
+                    <input type="text" className="input-box" />
+                </div>
+                <div>
+                    <label className="block text-sm font-bold">Position</label>
+                    <input type="text" className="input-box" />
+                </div>
+                <div>
+                    <label className="block text-sm font-bold">Contact Number</label>
+                    <input type="text" className="input-box" />
+                </div>
+                <div>
+                    <label className="block text-sm font-bold">Email Address</label>
+                    <input type="email" className="input-box" />
+                </div>
+                <div>
+                    <label className="block text-sm font-bold">City and Province</label>
+                    <input type="text" className="input-box" />
+                </div>
+                <div>
+                    <label className="block text-sm font-bold">Description</label>
+                    <textarea className="input-box"></textarea>
+                </div>
+            </form>
         </div>
     );
 }
