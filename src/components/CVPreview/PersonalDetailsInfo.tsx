5403cdf0071c615df4bdec237e23df1fa29ffc2b
Kyla <kflmorcillos@gmail.com>
2025-02-10 20:28:02 +0800

    PersonalDetailsInfo: Adjust margin

--- src/components/CVPreview/PersonalDetailsInfo.tsx
+++ src/components/CVPreview/PersonalDetailsInfo.tsx
@@ -8,8 +8,8 @@ export function PersonalDetailsInfo({ userInfo }: Props) {
     return(
         <div className="pb-4 text-sm">
             <p className="font-bold text-4xl mb-1">{userInfo.fullName}</p>
-            <p className="text-lg mb-6">{userInfo.position}</p>
-            <div className="grid grid-cols-3 mb-6 gap-y-2 w-full">
+            <p className="text-lg mb-2">{userInfo.position}</p>
+            <div className="grid grid-cols-3 mb-4 gap-y-2 w-full">
                 <p className="inline text-sm">{userInfo.contactNumber}</p>
                 <p className="inline text-sm">{userInfo.email}</p>
                 <p className="inline text-sm">{userInfo.cityProvince}</p>
