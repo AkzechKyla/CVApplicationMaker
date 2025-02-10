302f5380d366ac6f094cb2ebc5107f37809f545e
Kyla <kflmorcillos@gmail.com>
2025-02-10 21:35:03 +0800

    UserInfo: Add experiences

--- src/models/UserInfo.ts
+++ src/models/UserInfo.ts
@@ -1,3 +1,12 @@
+interface Experience {
+  positionName: string;
+  companyName: string;
+  startDate: string;
+  endDate?: string;
+  isPresent: boolean;
+  descriptions: string[];
+}
+
 export default class UserInfo {
   fullName: string;
   position: string;
@@ -5,13 +14,15 @@ export default class UserInfo {
   email: string;
   cityProvince: string;
   description: string;
+  experiences: Experience[];
 
-  constructor(fullName: string, position: string, contactNumber: string, email: string, cityProvince: string, description: string) {
+  constructor(fullName: string, position: string, contactNumber: string, email: string, cityProvince: string, description: string, experiences: Experience[] = []) {
     this.fullName = fullName;
     this.position = position;
     this.contactNumber = contactNumber;
     this.email = email;
     this.cityProvince = cityProvince;
     this.description = description;
+    this.experiences = experiences;
   }
 }
