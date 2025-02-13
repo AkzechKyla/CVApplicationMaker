6e85527020440257bfd5f0817bc73881a6089d84
Kyla <kflmorcillos@gmail.com>
2025-02-13 20:14:00 +0800

    SkillsForm: Add styles

--- src/components/CVForm/SkillsForm.tsx
+++ src/components/CVForm/SkillsForm.tsx
@@ -1,18 +1,30 @@
 function SkillsForm() {
     return(
         <div className="bg-white max-w-full flex-1 p-3 drop-shadow-md rounded-lg space-y-3">
-            <p>Skills</p>
+            <h1 className="font-bold">Skills</h1>
 
             <div className="flex justify-between">
-                <p>Skill 1</p>
-                <p>Delete</p>
+                <p className="text-base font-bold">Skill 1</p>
+                <button>
+                    <span className="material-icons text-red-600">delete</span>
+                </button>
             </div>
 
-            <div className="grid grid-cols-2 gap-2">
-                <p>Vendor Management</p>
-                <p>proven track record of managing vendors in projects with budgets of over $1,000,000.</p>
+            <form className="space-y-2">
+                <div>
+                    <label className="block text-sm font-bold">Skill Name</label>
+                    <input
+                        type="text"
+                        className="input-box"
+                    />
+                </div>
+            </form>
+
+            <div className="flex justify-center">
+                <button className="add-button">
+                    <span className="text-lg">+</span> Skill
+                </button>
             </div>
-            <p>+Add Skill</p>
         </div>
     );
 }
