b668a85971774e896bb393c16e42ac4e6f34b827
Kyla <kflmorcillos@gmail.com>
2025-02-17 18:42:30 +0800

    Redirect header title to GitHub repository

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,8 +1,15 @@
 function Title() {
     return(
-        <div className="flex mb-6 items-center bg-white p-4 drop-shadow-md rounded-lg justify-center">
+        <div className="flex mb-6 items-center bg-white p-4 drop-shadow-md rounded-lg justify-center select-none">
             <span className="material-icons text-5xl">description</span>
-            <h1 className="font-bold text-5xl ml-2">CV Application Maker</h1>
+            <h1 className="font-bold text-5xl ml-2">
+                <a
+                    href="https://github.com/AkzechKyla/CVApplicationMaker"
+                    target="_blank"
+                >
+                    CV Application Maker
+                </a>
+            </h1>
         </div>
     );
 }
